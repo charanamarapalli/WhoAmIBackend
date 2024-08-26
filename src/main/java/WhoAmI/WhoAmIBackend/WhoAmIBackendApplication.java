fd6eb61f02ce2ae1eb2e47f4ee81425a6875d148
Naidu Amarapalli <saicharannaiduamarapalli@gmail.com>
2024-08-26 20:13:45 +0530

    SpringBoot Project Setup

---
+++ src/main/java/WhoAmI/WhoAmIBackend/WhoAmIBackendApplication.java
@@ -0,0 +1,13 @@
+package WhoAmI.WhoAmIBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhoAmIBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhoAmIBackendApplication.class, args);
+	}
+
+}
